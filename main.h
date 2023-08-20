7f8ddb1610f94dda5f747745967a3bba2f996a87
YassinBoya <hondayassin5@gmail.com>
2023-08-20 13:57:40 -0700

    print-f.c

--- main.h
+++ main.h
@@ -3,8 +3,9 @@
 
 #include <stdlib.h>
 
-int _puts(char *str);
-int _putchar(char c);
+void handle_char(va_list args, int *sum_chars);
+void handle_string(va_list args, int *sum_chars);
+void handle_percent(int *sum_chars);
 int _printf(const char *format, ...);
 
 #endif
