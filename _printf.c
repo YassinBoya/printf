a4e7c26b40d9b0c7db098f162730f9c55050dbdc
YassinBoya <hondayassin5@gmail.com>
2023-08-20 11:44:10 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * _printf - function that produces output according to a format
+ * @format: the input to print
+ */
+int _printf(const char *format, ...)
+{
+	int i, sum_chars = 0;
+	const char *f = format;
+	va_list = args;
+
+	va_start(args , format);
+
+	if (!format)
+		return (-1);
+	for (*f = format; *f != "\0"; f++)
+	{
+		if(*f == "%")
+		{
+			f++;
+			if (*f == '\0')
+				break;
+			if(*f == 'c')
+			{
+				int chara = va_arg(args, int);
+				_putchar(chara);
+				sum_chars++;
+			}
+			else if (*f == 'S')
+			{
+				char str* = va_arg(args, char *);
+				for (i = 0; str[i] != '\0'; i++)
+				{
+					_putchar(str[i]);
+					sum_chars++;
+				}
+				else if (*f == '%')
+				{
+					_putchar('%');
+					sum_chars++;
+				}
+			}
+			else
+			{
+				_putchar(*f);
+				sum_chars++;
+			}
+		}
+		va_end(args);
+		return (sum_chars);
+	}
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef function_pointers_H
+#define function_pointers_H
+
+#include <stdlib.h>
+
+int _puts(char *str);
+int _putchr(char c);
+int _printf(const char *format, ...);
+
+#endif
